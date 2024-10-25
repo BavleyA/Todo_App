4b3ff95374e2d47523a48496a6a15957f2bf2cac
BavleyA <paula3del@gmail.com>
2024-10-25 14:40:27 +0300

    Create DataBase

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:todoapp/modules/archived_tasks/archived_tasks_screen.dart';
 import 'package:todoapp/modules/done_tasks/done_tasks_screen.dart';
 import 'package:todoapp/modules/new_tasks/new_tasks_screen.dart';
@@ -24,6 +25,12 @@ class _HomeLayoutState extends State<HomeLayout> {
     'Done Tasks',
     'Archived Tasks',
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    createDatabase();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,5 +76,33 @@ class _HomeLayoutState extends State<HomeLayout> {
         ],
       ),
     );
+  }
+
+
+  void createDatabase() async {
+    var database = await openDatabase(
+      'todo.db',
+      version: 1,
+      onCreate: (database,version)
+      {
+        print('Data Base Created');
+        // create tables
+        database.execute('CREATE TABLE tasks (id INTEGER PRIMARY KEY , title TEXT , time TEXT , status TEXT)').then((value) {
+          print('table created');
+        }).catchError((error){
+          print('error while creating table ${error.toString()}');
+        });
+
+      },
+
+      onOpen: (database){
+        print('DataBase opened');
+      },
+    );
+  }
+
+  void insertIntoDatabase(){
+
+
   }
 }
